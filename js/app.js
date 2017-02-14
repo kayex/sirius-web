823eaecf2da9cd75962870cf8a6710358b7df3f1
Johan Vester <jv@jv-macbook.local>
2017-02-14 13:14:54 +0100

    Use Sirius ID instead of Slack token in API requests

--- js/app.js
+++ js/app.js
@@ -7,17 +7,22 @@ app.controller('MainController', ['$scope', '$http', '$mdToast', function($scope
     $scope.saving = false;
     $scope.plugins = [];
     $scope.token = "";
+    $scope.siriusId = "";
 
-    if(localStorage.slack_token){
+    if (localStorage.slack_token) {
         $scope.token = localStorage.slack_token;
+    }
+
+    if (localStorage.sirius_id) {
+        $scope.siriusId = localStorage.sirius_id;
     }
 
     $http.get(BASEPATH + '/plugins').then(function(res){
         console.log(res.data);
         $scope.plugins = res.data;
 
-        if($scope.token !== ""){
-            $http.get(BASEPATH + '/configs/' + $scope.token).then(function(res){
+        if($scope.siriusId !== ""){
+            $http.get(BASEPATH + '/configs/' + $scope.siriusId).then(function(res){
                 let config = res.data;
                 console.log(config);
                 for (var i = 0; i < $scope.plugins.length; i++) {
@@ -72,8 +77,9 @@ app.controller('MainController', ['$scope', '$http', '$mdToast', function($scope
 
         $http.post(BASEPATH + '/configs', $scope.generateConfig()).then(function(res){
             console.log(res);
+            localStorage.setItem('sirius_id', res.data.sirius_id);
             localStorage.setItem('slack_token', res.data.slack_token);
-            $mdToast.show($mdToast.simple().textContent('Successful save!'));
+            $mdToast.show($mdToast.simple().textContent('Registration saved successfully!'));
         }, function(res){
             alert('An error occured!');
         }).finally(function(){
